a6ce5d63d596571adb3036fd7ab18044b5a812ee
deepa.k <deepa.k@cativ.in>
2026-05-20 15:25:13 +0530

    asn SP CHANGES

--- ASNHeaders.sql
+++ ASNHeaders.sql
@@ -1,212 +1,150 @@
 USE [Vertiv]
 GO
 
-/****** Object:  StoredProcedure [dbo].[SaveASNHeaderJson]    Script Date: 05-05-2026 11:06:27 ******/
+/****** Object:  StoredProcedure [dbo].[saveasnlineitemsjson_new]    Script Date: 20-05-2026 15:23:14 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-
-
-
-
-ALTER        PROCEDURE [dbo].[SaveASNHeaderJson]
-    @JsonData NVARCHAR(MAX)
-AS
-BEGIN
-    SET NOCOUNT ON;
-
-    BEGIN TRY
-
-        IF @JsonData IS NULL OR ISJSON(@JsonData) = 0
-        BEGIN
-            RAISERROR('Invalid or NULL JSON passed', 16, 1);
-            RETURN;
-        END
-
-        DECLARE @Result TABLE (
-            ASNNubmer VARCHAR(50)
-           
-        );
-
-        MERGE dbo.InvoiceASNDetails AS TARGET
-        USING (
-            SELECT
-                ASNNubmer,
-                Invoice_no,
-                ShippingDate,
-                ExpectedRecieptDate,
-                ModeOfTransport,
-                Remarks,
-                VehicleNo,
-                DriverName,
-                DriverLicenseNo,
-                DriverPhone,
-                LRNo,
-                LRDate,
-                TransportCompanyName,
-                TransmitTime,
-                RRNo,
-                RRDate,
-                AirwayBillNo,
-                FlightNo,
-                FlightDate,
-                AirlineNo,
-                BillofLadingNo,
-                BOLDate,
-                VesselName,
-                voyageNo,
-                CourierNo,
-                TrackingNo,
-                DockerNo
-            FROM OPENJSON(@JsonData)
-            WITH (
-                ASNNubmer VARCHAR(50),
-                Invoice_no VARCHAR(36),
-                ShippingDate DATETIME,
-                ExpectedRecieptDate DATETIME,
-                ModeOfTransport INT,
-                Remarks NVARCHAR(4000),
-                VehicleNo VARCHAR(50),
-                DriverName VARCHAR(100),
-                DriverLicenseNo VARCHAR(100),
-                DriverPhone VARCHAR(100),
-                LRNo VARCHAR(100),
-                LRDate DATETIME,
-                TransportCompanyName VARCHAR(100),
-                TransmitTime TIME(7),
-                RRNo VARCHAR(100),
-                RRDate DATE,
-                AirwayBillNo VARCHAR(100),
-                FlightNo VARCHAR(100),
-                FlightDate DATE,
-                AirlineNo VARCHAR(100),
-                BillofLadingNo VARCHAR(100),
-                BOLDate DATE,
-                VesselName VARCHAR(100),
-                voyageNo VARCHAR(100),
-                CourierNo VARCHAR(100),
-                TrackingNo VARCHAR(100),
-                DockerNo VARCHAR(100)
-            )
-        ) AS SOURCE
-        ON TARGET.ASNNubmer = SOURCE.ASNNubmer
-
-        WHEN MATCHED THEN
-            UPDATE SET
-                Invoice_no            = SOURCE.Invoice_no,
-                ShippingDate          = SOURCE.ShippingDate,
-                ExpectedRecieptDate   = SOURCE.ExpectedRecieptDate,
-                ModeOfTransport       = SOURCE.ModeOfTransport,
-                Remarks               = SOURCE.Remarks,
-                VehicleNo             = SOURCE.VehicleNo,
-                DriverName            = SOURCE.DriverName,
-                DriverLicenseNo       = SOURCE.DriverLicenseNo,
-                DriverPhone           = SOURCE.DriverPhone,
-                LRNo                  = SOURCE.LRNo,
-                LRDate                = SOURCE.LRDate,
-                TransportCompanyName  = SOURCE.TransportCompanyName,
-                TransmitTime          = SOURCE.TransmitTime,
-                RRNo                  = SOURCE.RRNo,
-                RRDate                = SOURCE.RRDate,
-                AirwayBillNo          = SOURCE.AirwayBillNo,
-                FlightNo              = SOURCE.FlightNo,
-                FlightDate            = SOURCE.FlightDate,
-                AirlineNo             = SOURCE.AirlineNo,
-                BillofLadingNo        = SOURCE.BillofLadingNo,
-                BOLDate               = SOURCE.BOLDate,
-                VesselName            = SOURCE.VesselName,
-                voyageNo              = SOURCE.voyageNo,
-                CourierNo             = SOURCE.CourierNo,
-                TrackingNo            = SOURCE.TrackingNo,
-                DockerNo              = SOURCE.DockerNo
-
-        WHEN NOT MATCHED THEN
-            INSERT (
-                ASNId,
-                ASNNubmer,
-                Invoice_no,
-                ShippingDate,
-                ExpectedRecieptDate,
-                ModeOfTransport,
-                Remarks,
-                VehicleNo,
-                DriverName,
-                DriverLicenseNo,
-                DriverPhone,
-                LRNo,
-                LRDate,
-                TransportCompanyName,
-                TransmitTime,
-                RRNo,
-                RRDate,
-                AirwayBillNo,
-                FlightNo,
-                FlightDate,
-                AirlineNo,
-                BillofLadingNo,
-                BOLDate,
-                VesselName,
-                voyageNo,
-                CourierNo,
-                TrackingNo,
-                DockerNo
-            )
-            VALUES (
-                NEWID(),
-                SOURCE.ASNNubmer,
-                SOURCE.Invoice_no,
-                SOURCE.ShippingDate,
-                SOURCE.ExpectedRecieptDate,
-                SOURCE.ModeOfTransport,
-                SOURCE.Remarks,
-                SOURCE.VehicleNo,
-                SOURCE.DriverName,
-                SOURCE.DriverLicenseNo,
-                SOURCE.DriverPhone,
-                SOURCE.LRNo,
-                SOURCE.LRDate,
-                SOURCE.TransportCompanyName,
-                SOURCE.TransmitTime,
-                SOURCE.RRNo,
-                SOURCE.RRDate,
-                SOURCE.AirwayBillNo,
-                SOURCE.FlightNo,
-                SOURCE.FlightDate,
-                SOURCE.AirlineNo,
-                SOURCE.BillofLadingNo,
-                SOURCE.BOLDate,
-                SOURCE.VesselName,
-                SOURCE.voyageNo,
-                SOURCE.CourierNo,
-                SOURCE.TrackingNo,
-                SOURCE.DockerNo
-            )
-
-        OUTPUT
-            INSERTED.ASNNubmer AS ASN_NUMBER
+ALTER      procedure [dbo].[saveasnlineitemsjson_new]       
+    @jsondata nvarchar(max)        
+as        
+begin        
+    set nocount on;        
+        
+        
+    --   validate json    
+        if @jsondata is null or isjson(@jsondata) = 0    
+        begin    
+            throw 50001, 'invalid or null json passed to savegrnlineitemsjson', 1;    
+        end    
+  
+  insert into vertiv..pojsonfromerp        
+  values('before asn insert lineitem',@jsondata,getdate())       
+            
+      
+   declare @templineitems table      
+   (      
+                asnlineitemid varchar(36),  
+                asnid varchar(36),  
+                [lineno] int,  
+                itemcode varchar(50),  
+                itemdesc nvarchar(4000),  
+                uom varchar(50),  
+                poqty decimal(18,2),  
+                openqty decimal(18,2),  
+                shippedqty decimal(18,2),  
+                noofpackage int,  
+                batchno varchar(100)      
+                   
+  )      
+  insert into @templineitems      
+  (      
+               asnlineitemid,  
+                asnid,  
+                [lineno],  
+                itemcode,  
+                itemdesc,  
+                uom,  
+                poqty,  
+                openqty,  
+                shippedqty,  
+                noofpackage,  
+                batchno       
+  )      
          
-        INTO @Result;
-		declare @ponumber varchar(50)
-		select @ponumber=ASNNubmer
-		from	@Result
+        select   
+        
+		        asnlineitemid,  
+                asnid,  
+                [lineno],  
+                itemcode,  
+                itemdesc,  
+                uom,  
+                poqty,  
+                openqty,  
+                shippedqty,  
+                noofpackage,  
+                batchno        
+                      
+        from openjson(@jsondata)        
+        with (        
+                asnlineitemid varchar(36),  
+                asnid varchar(36),  
+                [lineno] int,  
+                itemcode varchar(50),  
+                itemdesc nvarchar(4000),  
+                uom varchar(50),  
+                poqty decimal(18,2),  
+                openqty decimal(18,2),  
+                shippedqty decimal(18,2),  
+                noofpackage int,  
+                batchno varchar(100)      
+        )        
+          
+   
+  --update existing line items      
+   update tar      
+   set        
+            
+         
+		        asnid        = src.asnid,  
+                [lineno]     = src.[lineno],  
+                itemcode     = src.itemcode,  
+                itemdesc     = src.itemdesc,  
+                uom          = src.uom,  
+                poqty        = src.poqty,  
+                openqty      = src.openqty,  
+                shippedqty   = src.shippedqty,  
+                noofpackage  = src.noofpackage,  
+                batchno      = src.batchno  
+		  
+ from @templineitems src      
+ join vertiv..asnlineitems tar       
+ on  src.asnlineitemid=tar.asnlineitemid          
+      
+ --insert new line items      
+      
+        insert  into vertiv..asnlineitems      
+  (        
+             asnlineitemid,  
+                asnid,  
+                [lineno],  
+                itemcode,  
+                itemdesc,  
+                uom,  
+                poqty,  
+                openqty,  
+                shippedqty,  
+                noofpackage,  
+                batchno        
+        )       
+  select    
+                src.asnlineitemid,  
+                src.asnid,  
+                src.[lineno],  
+                src.itemcode,  
+                src.itemdesc,  
+                src.uom,  
+                src.poqty,  
+                src.openqty,  
+                src.shippedqty,  
+                src.noofpackage,  
+                src.batchno        
+                 
+  from @templineitems src
+left join vertiv..asnlineitems pli
+    on src.asnlineitemid = pli.asnlineitemid
+where pli.asnlineitemid is null;     
+      
+            
+  insert into vertiv..pojsonfromerp        
+  values('asnlineitem',@jsondata,getdate())        
+end 
 
-		insert into POJsonfromERP
-		values(@ponumber,@JsonData,getdate())
 
-		SELECT ASNNubmer AS ASN_NUMBER
-FROM @Result;
-
-    END TRY
-    BEGIN CATCH
-        SELECT
-            'ERROR' AS Status,
-            ERROR_MESSAGE() AS ErrorMessage,
-            ERROR_LINE() AS ErrorLine;
-    END CATCH
-END;
 GO
 
 
--- ASNLineItems.sql
+++ ASNLineItems.sql
@@ -1,131 +1,148 @@
 USE [Vertiv]
 GO
 
-/****** Object:  StoredProcedure [dbo].[SaveASNLineItemsJson]    Script Date: 05-05-2026 11:06:06 ******/
+/****** Object:  StoredProcedure [dbo].[SaveASNLineItemsJson]    Script Date: 20-05-2026 15:24:41 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-
-
-
-CREATE     PROCEDURE [dbo].[SaveASNLineItemsJson]
-    @JsonData NVARCHAR(MAX)
-AS
-BEGIN
-    SET NOCOUNT ON;
-
-    BEGIN TRY
-
-        -- Validate JSON
-        IF @JsonData IS NULL OR ISJSON(@JsonData) = 0
-        BEGIN
-            RAISERROR('Invalid or NULL JSON passed to SaveASNLineItemsJson', 16, 1);
-            RETURN;
-        END
-
-        DECLARE @Result TABLE (
-            ASNLineItemId VARCHAR(36),
-            ASNId VARCHAR(36)
-        );
-
-        -- UPSERT using MERGE
-        MERGE dbo.ASNLineItems AS TARGET
-        USING (
-            SELECT
-                ASNLineItemId,
-                ASNId,
-                [LineNo],
-                ItemCode,
-                ItemDesc,
-                UOM,
-                POQty,
-                OpenQty,
-                ShippedQty,
-                NoOfPackage,
-                BatchNo
-            FROM OPENJSON(@JsonData)
-            WITH (
-                ASNLineItemId VARCHAR(36),
-                ASNId VARCHAR(36),
-                [LineNo] INT,
-                ItemCode VARCHAR(50),
-                ItemDesc NVARCHAR(4000),
-                UOM VARCHAR(50),
-                POQty DECIMAL(18,2),
-                OpenQty DECIMAL(18,2),
-                ShippedQty DECIMAL(18,2),
-                NoOfPackage INT,
-                BatchNo VARCHAR(100)
-            )
-        ) AS SOURCE
-        ON TARGET.ASNLineItemId = SOURCE.ASNLineItemId
-
-        WHEN MATCHED THEN
-            UPDATE SET
-                ASNId        = SOURCE.ASNId,
-                [LineNo]     = SOURCE.[LineNo],
-                ItemCode     = SOURCE.ItemCode,
-                ItemDesc     = SOURCE.ItemDesc,
-                UOM          = SOURCE.UOM,
-                POQty        = SOURCE.POQty,
-                OpenQty      = SOURCE.OpenQty,
-                ShippedQty   = SOURCE.ShippedQty,
-                NoOfPackage  = SOURCE.NoOfPackage,
-                BatchNo      = SOURCE.BatchNo
-
-        WHEN NOT MATCHED THEN
-            INSERT (
-                ASNLineItemId,
-                ASNId,
-                [LineNo],
-                ItemCode,
-                ItemDesc,
-                UOM,
-                POQty,
-                OpenQty,
-                ShippedQty,
-                NoOfPackage,
-                BatchNo
-            )
-            VALUES (
-                SOURCE.ASNLineItemId,
-                SOURCE.ASNId,
-                SOURCE.[LineNo],
-                SOURCE.ItemCode,
-                SOURCE.ItemDesc,
-                SOURCE.UOM,
-                SOURCE.POQty,
-                SOURCE.OpenQty,
-                SOURCE.ShippedQty,
-                SOURCE.NoOfPackage,
-                SOURCE.BatchNo
-            )
-
-        OUTPUT
-            INSERTED.ASNLineItemId,
-            INSERTED.ASNId
-        INTO @Result;
-
-        -- Return response
-		insert into POJsonfromERP
-		values('ASNlineitem',@JsonData,getdate()) 
-        SELECT
-            'SUCCESS' AS Status,
-            ASNLineItemId
+  ALTER      procedure [dbo].[SaveASNLineItemsJson]       
+    @jsondata nvarchar(max)        
+as        
+begin        
+    set nocount on;        
+        
+        
+    --   validate json    
+        if @jsondata is null or isjson(@jsondata) = 0    
+        begin    
+            throw 50001, 'invalid or null json passed to savegrnlineitemsjson', 1;    
+        end    
+  
+  insert into vertiv..pojsonfromerp        
+  values('before ASN insert lineitem',@jsondata,getdate())       
+            
+      
+   declare @templineitems table      
+   (      
+                ASNLineItemId VARCHAR(36),  
+                ASNId VARCHAR(36),  
+                [LineNo] INT,  
+                ItemCode VARCHAR(50),  
+                ItemDesc NVARCHAR(4000),  
+                UOM VARCHAR(50),  
+                POQty DECIMAL(18,2),  
+                OpenQty DECIMAL(18,2),  
+                ShippedQty DECIMAL(18,2),  
+                NoOfPackage INT,  
+                BatchNo VARCHAR(100)      
+                   
+  )      
+  insert into @templineitems      
+  (      
+               ASNLineItemId,  
+                ASNId,  
+                [LineNo],  
+                ItemCode,  
+                ItemDesc,  
+                UOM,  
+                POQty,  
+                OpenQty,  
+                ShippedQty,  
+                NoOfPackage,  
+                BatchNo       
+  )      
+         
+        select   
+        
+		        ASNLineItemId,  
+                ASNId,  
+                [LineNo],  
+                ItemCode,  
+                ItemDesc,  
+                UOM,  
+                POQty,  
+                OpenQty,  
+                ShippedQty,  
+                NoOfPackage,  
+                BatchNo        
+                      
+        from openjson(@jsondata)        
+        with (        
+                ASNLineItemId VARCHAR(36),  
+                ASNId VARCHAR(36),  
+                [LineNo] INT,  
+                ItemCode VARCHAR(50),  
+                ItemDesc NVARCHAR(4000),  
+                UOM VARCHAR(50),  
+                POQty DECIMAL(18,2),  
+                OpenQty DECIMAL(18,2),  
+                ShippedQty DECIMAL(18,2),  
+                NoOfPackage INT,  
+                BatchNo VARCHAR(100)      
+        )        
           
-        FROM @Result;
-
-    END TRY
-    BEGIN CATCH
-        SELECT
-            'ERROR' AS Status,
-            ERROR_MESSAGE() AS ErrorMessage,
-            ERROR_LINE() AS ErrorLine;
-    END CATCH
-END;
+   
+  --update existing line items      
+   update tar      
+   set        
+            
+         
+		        ASNId        = SRC.ASNId,  
+                [LineNo]     = SRC.[LineNo],  
+                ItemCode     = SRC.ItemCode,  
+                ItemDesc     = SRC.ItemDesc,  
+                UOM          = SRC.UOM,  
+                POQty        = SRC.POQty,  
+                OpenQty      = SRC.OpenQty,  
+                ShippedQty   = SRC.ShippedQty,  
+                NoOfPackage  = SRC.NoOfPackage,  
+                BatchNo      = SRC.BatchNo  
+		  
+ from @templineitems src      
+ join vertiv..ASNLineItems tar       
+ on  src.ASNLineItemId=tar.ASNLineItemId          
+      
+ --insert new line items      
+      
+        insert  into vertiv..ASNLineItems      
+  (        
+             ASNLineItemId,  
+                ASNId,  
+                [LineNo],  
+                ItemCode,  
+                ItemDesc,  
+                UOM,  
+                POQty,  
+                OpenQty,  
+                ShippedQty,  
+                NoOfPackage,  
+                BatchNo        
+        )       
+  select    
+                src.ASNLineItemId,  
+                src.ASNId,  
+                src.[LineNo],  
+                src.ItemCode,  
+                src.ItemDesc,  
+                src.UOM,  
+                src.POQty,  
+                src.OpenQty,  
+                src.ShippedQty,  
+                src.NoOfPackage,  
+                src.BatchNo        
+                 
+  from @templineitems src
+left join vertiv..ASNLineItems pli
+    on src.ASNLineItemId = pli.ASNLineItemId
+where pli.ASNLineItemId is null;     
+      
+            
+  insert into vertiv..pojsonfromerp        
+  values('ASNlineitem',@jsondata,getdate())        
+end 
 GO
 
 
