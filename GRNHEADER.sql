7bb3253b1fe4f0610d9ca63f656c880bed9e6c03
deepa.k <deepa.k@cativ.in>
2026-05-20 16:13:44 +0530

    GRN ADDED

--- GRNHEADER.sql
+++ GRNHEADER.sql
@@ -1,200 +1,181 @@
-USE [Vertiv]
-GO
-
-/****** Object:  StoredProcedure [dbo].[SaveGRNHeaderJson]    Script Date: 05-05-2026 11:01:04 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-
-
-ALTER PROCEDURE [dbo].[SaveGRNHeaderJson]
-    @JsonData NVARCHAR(MAX)
-AS
-BEGIN
-    SET NOCOUNT ON;
-
-    BEGIN TRY
-        BEGIN TRAN;
-
+alter    PROCEDURE [dbo]. SaveGRNHeaderJson    
+    @JsonData NVARCHAR(MAX)    
+AS    
+BEGIN    
+    SET NOCOUNT ON;    
+  
+    BEGIN TRY    
+    
+        IF @JsonData IS NULL OR ISJSON(@JsonData) = 0    
+        BEGIN    
+            RAISERROR('Invalid or NULL JSON passed', 16, 1);    
+            RETURN;    
+        END;    
+     
+        INSERT INTO POJsonfromERP    
+        VALUES ('GRN Before insert', @JsonData, GETDATE());    
+  
+          
+        DECLARE @TempGRNHeader TABLE    
+        (    
+            grn_id VARCHAR(36),    
+            grn_number VARCHAR(30),    
+            invoice_no VARCHAR(50),    
+            receipt_date DATETIME,    
+            po_number VARCHAR(20),    
+            vendor_name VARCHAR(200),    
+            vendor_gstin VARCHAR(15),    
+            ship_to_location VARCHAR(150),    
+            receipt_source VARCHAR(20),    
+            bill_of_lading VARCHAR(50),    
+            packing_slip VARCHAR(50),    
+            waybill_number VARCHAR(50),    
+            freight_carrier VARCHAR(100),    
+            num_of_containers INT    
+        );    
+  
+           
+         INSERT INTO @TempGRNHeader    
+  (    
+   grn_id,    
+                grn_number,    
+                invoice_no,    
+                receipt_date,    
+                po_number,    
+                vendor_name,    
+                vendor_gstin,    
+                ship_to_location,    
+                receipt_source,    
+                bill_of_lading,    
+                packing_slip,    
+                waybill_number,    
+                freight_carrier,    
+                num_of_containers    
+  )    
+    SELECT     
+                grn_id,    
+                grn_number,    
+                invoice_no,    
+                receipt_date,    
+                po_number,    
+                vendor_name,    
+                vendor_gstin,    
+                ship_to_location,    
+                receipt_source,    
+                bill_of_lading,    
+                packing_slip,    
+                waybill_number,    
+                freight_carrier,    
+                num_of_containers    
+            FROM OPENJSON(@JsonData)    
+            WITH (    
+                grn_id   VARCHAR(36),    
+            grn_number VARCHAR(30),    
+            invoice_no VARCHAR(50),    
+            receipt_date DATETIME,    
+            po_number VARCHAR(20),    
+            vendor_name VARCHAR(200),    
+            vendor_gstin VARCHAR(15),    
+            ship_to_location VARCHAR(150),    
+            receipt_source VARCHAR(20),    
+            bill_of_lading VARCHAR(50),    
+            packing_slip VARCHAR(50),    
+            waybill_number VARCHAR(50),    
+            freight_carrier VARCHAR(100),    
+            num_of_containers INT    
+    )    
          
-        IF @JsonData IS NULL OR ISJSON(@JsonData) = 0
-        BEGIN
-            THROW 50001, 'Invalid or NULL JSON passed', 1;
-        END
-		 
-        DECLARE @Source TABLE (
-            grn_id   VARCHAR(36),
-            grn_number VARCHAR(30),
-            invoice_no VARCHAR(50),
-            receipt_date DATETIME,
-            po_number VARCHAR(20),
-            vendor_name VARCHAR(200),
-            vendor_gstin VARCHAR(15),
-            ship_to_location VARCHAR(150),
-            receipt_source VARCHAR(20),
-            bill_of_lading VARCHAR(50),
-            packing_slip VARCHAR(50),
-            waybill_number VARCHAR(50),
-            freight_carrier VARCHAR(100),
-            num_of_containers INT
-        );
-
-        INSERT INTO @Source
-        SELECT
-            CASE 
-                WHEN grn_id IS NULL OR grn_id = '' 
-                    THEN CAST(NEWID() AS VARCHAR(36))
-                ELSE grn_id
-            END,
-            grn_number,
-            NULLIF(invoice_no, ''),
-            TRY_CAST(receipt_date AS DATETIME),
-            po_number,
-            vendor_name,
-            vendor_gstin,
-            ship_to_location,
-            receipt_source,
-            bill_of_lading,
-            packing_slip,
-            waybill_number,
-            freight_carrier,
-            TRY_CAST(num_of_containers AS INT)
-        FROM OPENJSON(@JsonData, '$')
-        WITH (
-            grn_id VARCHAR(36),
-            grn_number VARCHAR(30),
-            invoice_no VARCHAR(50),
-            receipt_date NVARCHAR(50),
-            po_number VARCHAR(20),
-            vendor_name VARCHAR(200),
-            vendor_gstin VARCHAR(15),
-            ship_to_location VARCHAR(150),
-            receipt_source VARCHAR(20),
-            bill_of_lading VARCHAR(50),
-            packing_slip VARCHAR(50),
-            waybill_number VARCHAR(50),
-            freight_carrier VARCHAR(100),
-            num_of_containers NVARCHAR(20)
-        );
-
-           DECLARE @InsertedGRNs TABLE (
-            GRN_NUMBER VARCHAR(30),
-            GRNCreatedOn DATETIME
-        );
-
-        MERGE dbo.GRNHeader AS TARGET
-        USING @Source AS SOURCE
-        ON TARGET.grn_id = SOURCE.grn_id
-
-        WHEN MATCHED THEN
-            UPDATE SET
-                grn_number        = SOURCE.grn_number,
-                invoice_no        = SOURCE.invoice_no,
-                receipt_date      = SOURCE.receipt_date,
-                po_number         = SOURCE.po_number,
-                vendor_name       = SOURCE.vendor_name,
-                vendor_gstin      = SOURCE.vendor_gstin,
-                ship_to_location  = SOURCE.ship_to_location,
-                receipt_source    = SOURCE.receipt_source,
-                bill_of_lading    = SOURCE.bill_of_lading,
-                packing_slip      = SOURCE.packing_slip,
-                waybill_number    = SOURCE.waybill_number,
-                freight_carrier   = SOURCE.freight_carrier,
-                num_of_containers = SOURCE.num_of_containers
-
-        WHEN NOT MATCHED THEN
-            INSERT (
-                grn_id,
-                grn_number,
-                invoice_no,
-                receipt_date,
-                po_number,
-                vendor_name,
-                vendor_gstin,
-                ship_to_location,
-                receipt_source,
-                bill_of_lading,
-                packing_slip,
-                waybill_number,
-                freight_carrier,
-                num_of_containers
-            )
-            VALUES (
-                SOURCE.grn_id,
-                SOURCE.grn_number,
-                SOURCE.invoice_no,
-                SOURCE.receipt_date,
-                SOURCE.po_number,
-                SOURCE.vendor_name,
-                SOURCE.vendor_gstin,
-                SOURCE.ship_to_location,
-                SOURCE.receipt_source,
-                SOURCE.bill_of_lading,
-                SOURCE.packing_slip,
-                SOURCE.waybill_number,
-                SOURCE.freight_carrier,
-                SOURCE.num_of_containers
-            )
-
-        OUTPUT
-            INSERTED.grn_number,
-            INSERTED.created_at
-        INTO @InsertedGRNs;
-
-        COMMIT TRAN;
-
-        -- Insert log
-        DECLARE @ponumber VARCHAR(50);
-
-        SELECT TOP 1 @ponumber = GRN_NUMBER
-        FROM @InsertedGRNs;
-
-        INSERT INTO POJsonfromERP
-        VALUES (@ponumber, @JsonData, GETDATE());
-
-        -- Return result
-        SELECT * FROM @InsertedGRNs;
-
-		declare @loop int,				
-				@loopcount int,
-				@tempgrnnumber varchar(30)
-
-		select @loopcount=count(*) from @InsertedGRNs
-
-		set @loop=1
-		while(@loop<=@loopcount)
-		begin
-			select top 1 @tempgrnnumber=GRN_NUMBER
-			from @InsertedGRNs
-			if(@tempgrnnumber <>'')
-			begin
-				exec vertiv..GenerateDossierGRN @tempgrnnumber
-			end
-
-			delete from @InsertedGRNs where GRN_NUMBER=@tempgrnnumber
-
-			set @tempgrnnumber=''
-
-			set @loop=@loop+1
-		end
-
-    END TRY
-    BEGIN CATCH
-        IF @@TRANCOUNT > 0
-            ROLLBACK TRAN;
-
-        --  
-        SELECT 
-            ERROR_MESSAGE() AS ErrorMessage,
-            ERROR_LINE() AS ErrorLine,
-            ERROR_PROCEDURE() AS ErrorProcedure;
-
-        THROW;
-    END CATCH
-END;
-GO
-
-
+          
+        -- Update Existing  
+          
+        UPDATE TAR    
+        SET    
+            grn_number        = SRC.grn_number,    
+            invoice_no        = SRC.invoice_no,    
+            receipt_date      = SRC.receipt_date,    
+            po_number         = SRC.po_number,    
+            vendor_name       = SRC.vendor_name,    
+            vendor_gstin      = SRC.vendor_gstin,    
+            ship_to_location  = SRC.ship_to_location,    
+            receipt_source    = SRC.receipt_source,    
+            bill_of_lading    = SRC.bill_of_lading,    
+            packing_slip      = SRC.packing_slip,    
+            waybill_number    = SRC.waybill_number,    
+            freight_carrier   = SRC.freight_carrier,    
+            num_of_containers = SRC.num_of_containers    
+        FROM Vertiv..GRNHeader TAR    
+        JOIN @TempGRNHeader SRC    
+            ON TAR.grn_id = SRC.grn_id;    
+  
+         -- Insert New Records  
+           
+        INSERT INTO Vertiv..GRNHeader    
+        (    
+            grn_id, grn_number, invoice_no, receipt_date, po_number,    
+            vendor_name, vendor_gstin, ship_to_location, receipt_source,    
+            bill_of_lading, packing_slip, waybill_number, freight_carrier, num_of_containers    
+        )    
+        SELECT    
+        TPO.grn_id,    
+                TPO.grn_number,    
+                TPO.invoice_no,    
+                TPO.receipt_date,    
+                TPO.po_number,    
+                TPO.vendor_name,    
+                TPO.vendor_gstin,    
+                TPO.ship_to_location,    
+                TPO.receipt_source,    
+                TPO.bill_of_lading,    
+                TPO.packing_slip,    
+                TPO.waybill_number,    
+                TPO.freight_carrier,    
+                TPO.num_of_containers    
+        FROM @TempGRNHeader TPO    
+        LEFT JOIN Vertiv..GRNHeader PO    
+            ON TPO.grn_id = PO.grn_id    
+        WHERE PO.grn_id IS NULL;    
+  
+         DECLARE @InsertedGRNs TABLE    
+        (    
+            grn_number VARCHAR(30),    
+            GRNCreatedOn DATETIME    
+        );    
+    
+        SELECT  grn_number  AS GRN_NUMBER  
+  FROM @TempGRNHeader;    
+    
+   INSERT INTO POJsonfromERP (PONUMBER, JsonData, createdat)    
+OUTPUT INSERTED.PONUMBER, INSERTED.createdat INTO @InsertedGRNs    
+   
+  
+        SELECT grn_number, @JsonData, GETDATE()    
+        FROM @TempGRNHeader;    
+  
+         SELECT * FROM @InsertedGRNs;    
+     
+           
+        -- Loop & Call Procedure   
+        DECLARE @tempgrnnumber VARCHAR(30);    
+  
+        WHILE EXISTS (SELECT 1 FROM @InsertedGRNs)    
+        BEGIN    
+            SELECT TOP 1 @tempgrnnumber = grn_number    
+            FROM @InsertedGRNs;    
+  
+            IF (@tempgrnnumber IS NOT NULL)    
+            BEGIN    
+                EXEC vertiv..GenerateDossierGRN @tempgrnnumber;    
+            END    
+  
+            DELETE FROM @InsertedGRNs    
+            WHERE grn_number = @tempgrnnumber;    
+        END;    
+  
+    END TRY    
+    BEGIN CATCH    
+        SELECT    
+            'ERROR' AS Status,    
+            ERROR_MESSAGE() AS ErrorMessage,    
+            ERROR_LINE() AS ErrorLine;    
+    END CATCH    
+END;  
--- GRNLineItems.sql
+++ GRNLineItems.sql
@@ -1,177 +1,192 @@
-﻿USE [Vertiv]
-GO
-
-/****** Object:  StoredProcedure [dbo].[SaveGRNLineItemsJson]    Script Date: 05-05-2026 11:04:23 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-
-ALTER PROCEDURE [dbo].[SaveGRNLineItemsJson]
-    @JsonData NVARCHAR(MAX)
-AS
-BEGIN
-    SET NOCOUNT ON;
-
-    BEGIN TRY
-
-        --   Validate JSON
-        IF @JsonData IS NULL OR ISJSON(@JsonData) = 0
-        BEGIN
-            THROW 50001, 'Invalid or NULL JSON passed to SaveGRNLineItemsJson', 1;
-        END
-		 
-        MERGE GRNLineItems AS TARGET
-        USING (
-            SELECT
-                grn_id,
-                line_num,
-                po_line_num,
-                po_shipment_num,
-                item_code,
-                item_desc,
-                qty_ordered,
-                qty_received,
-                qty_accepted,
-                qty_rejected,
-                uom,
-                grn_line_id,
-                unit_price,
-                line_amount,
-                destination_type,
-                subinventory,
-                locator_id,
-                inspection_status,
-
-                 
-                ISNULL(TRY_CAST(NULLIF(created_at, '') AS DATETIME), GETDATE()) AS created_at,
-                TRY_CAST(NULLIF(updated_at, '') AS DATETIME) AS updated_at,
-                TRY_CAST(NULLIF(deleted_at, '') AS DATETIME) AS deleted_at
-
-            FROM OPENJSON(@JsonData, '$')
-            WITH (
-                grn_id  VARCHAR(36),
-                line_num INT,
-                po_line_num INT,
-                po_shipment_num INT,
-                item_code VARCHAR(50),
-                item_desc NVARCHAR(MAX),
-                qty_ordered DECIMAL(18,2),
-                qty_received DECIMAL(18,2),
-                qty_accepted DECIMAL(18,2),
-                qty_rejected DECIMAL(18,2),
-                uom VARCHAR(10),
-                grn_line_id VARCHAR(36),
-                unit_price DECIMAL(18,2),
-                line_amount DECIMAL(18,2),
-                destination_type VARCHAR(25),
-                subinventory VARCHAR(30),
-                locator_id INT,
-                inspection_status VARCHAR(20),
-
-                -- ✅ IMPORTANT: keep as NVARCHAR
-                created_at NVARCHAR(50),
-                updated_at NVARCHAR(50),
-                deleted_at NVARCHAR(50)
-            )
-        ) AS SOURCE
-
-        ON TARGET.grn_id = SOURCE.grn_id
-           AND TARGET.line_num = SOURCE.line_num
-
-        --  UPDATE
-        WHEN MATCHED THEN
-            UPDATE SET
-                po_line_num       = SOURCE.po_line_num,
-                po_shipment_num   = SOURCE.po_shipment_num,
-                item_code         = SOURCE.item_code,
-                item_desc         = SOURCE.item_desc,
-                qty_ordered       = SOURCE.qty_ordered,
-                qty_received      = SOURCE.qty_received,
-                qty_accepted      = SOURCE.qty_accepted,
-                qty_rejected      = SOURCE.qty_rejected,
-                uom               = SOURCE.uom,
-                grn_line_id       = SOURCE.grn_line_id,
-                unit_price        = SOURCE.unit_price,
-                line_amount       = SOURCE.line_amount,
-                destination_type  = SOURCE.destination_type,
-                subinventory      = SOURCE.subinventory,
-                locator_id        = SOURCE.locator_id,
-                inspection_status = SOURCE.inspection_status,
-                updated_at        = GETDATE()
-
-        --   INSERT
-        WHEN NOT MATCHED THEN
-            INSERT (
-                grn_id,
-                line_num,
-                po_line_num,
-                po_shipment_num,
-                item_code,
-                item_desc,
-                qty_ordered,
-                qty_received,
-                qty_accepted,
-                qty_rejected,
-                uom,
-                grn_line_id,
-                unit_price,
-                line_amount,
-                destination_type,
-                subinventory,
-                locator_id,
-                inspection_status,
-                created_at,
-                updated_at,
-                deleted_at
-            )
-            VALUES (
-                SOURCE.grn_id,
-                SOURCE.line_num,
-                SOURCE.po_line_num,
-                SOURCE.po_shipment_num,
-                SOURCE.item_code,
-                SOURCE.item_desc,
-                SOURCE.qty_ordered,
-                SOURCE.qty_received,
-                SOURCE.qty_accepted,
-                SOURCE.qty_rejected,
-                SOURCE.uom,
-                SOURCE.grn_line_id,
-                SOURCE.unit_price,
-                SOURCE.line_amount,
-                SOURCE.destination_type,
-                SOURCE.subinventory,
-                SOURCE.locator_id,
-                SOURCE.inspection_status,
-                SOURCE.created_at,
-                ISNULL(SOURCE.updated_at, GETDATE()),
-                SOURCE.deleted_at
-            );
-
-         
-        INSERT INTO POJsonfromERP
-        VALUES ('GRNlineitem', @JsonData, GETDATE());
-
-         
-        SELECT 
-            'SUCCESS' AS Status,
-            'Line items upserted successfully' AS Message;
-
-    END TRY
-    BEGIN CATCH
-
+﻿CREATE      PROCEDURE [dbo].SaveGRNLineItemsJson         
+    @JsonData NVARCHAR(MAX)          
+AS          
+BEGIN          
+    SET NOCOUNT ON;          
+          
+          
+    --   Validate JSON      
+        IF @JsonData IS NULL OR ISJSON(@JsonData) = 0      
+        BEGIN      
+            THROW 50001, 'Invalid or NULL JSON passed to SaveGRNLineItemsJson', 1;      
+        END      
+    
+  insert into vertiv..POJsonfromERP          
+  values('Before grn insert lineitem',@JsonData,getdate())         
+              
         
-        SELECT 
-            'ERROR' AS Status,
-            ERROR_MESSAGE() AS Message,
-            ERROR_LINE() AS ErrorLine,
-            ERROR_PROCEDURE() AS ErrorProcedure;
-
-    END CATCH
-END;
-GO
-
-
+   DECLARE @TempLineItems TABLE        
+   (        
+     grn_id  VARCHAR(36),      
+                line_num INT,      
+                po_line_num INT,      
+                po_shipment_num INT,      
+                item_code VARCHAR(50),      
+                item_desc NVARCHAR(MAX),      
+                qty_ordered DECIMAL(18,2),      
+                qty_received DECIMAL(18,2),      
+                qty_accepted DECIMAL(18,2),      
+                qty_rejected DECIMAL(18,2),      
+                uom VARCHAR(10),      
+                grn_line_id VARCHAR(36),      
+                unit_price DECIMAL(18,2),      
+                line_amount DECIMAL(18,2),      
+                destination_type VARCHAR(25),      
+                subinventory VARCHAR(30),      
+                locator_id INT,      
+                inspection_status VARCHAR(20),      
+                created_at datetime,      
+                updated_at datetime,    
+                deleted_at datetime        
+  )        
+  INSERT INTO @TempLineItems        
+  (        
+                grn_id,      
+                line_num,      
+                po_line_num,      
+                po_shipment_num,      
+                item_code,      
+                item_desc,      
+                qty_ordered,      
+                qty_received,      
+                qty_accepted,      
+                qty_rejected,      
+                uom,      
+                grn_line_id,      
+                unit_price,      
+                line_amount,      
+                destination_type,      
+                subinventory,      
+                locator_id,      
+                inspection_status,      
+                created_at,      
+              updated_at,      
+                deleted_at         
+  )        
+           
+        SELECT     
+          grn_id,      
+                line_num,      
+                po_line_num,      
+                po_shipment_num,      
+                item_code,      
+                item_desc,      
+                qty_ordered,      
+                qty_received,      
+                qty_accepted,      
+                qty_rejected,      
+                uom,      
+                grn_line_id,      
+                unit_price,      
+                line_amount,      
+                destination_type,      
+                subinventory,      
+                locator_id,      
+                inspection_status,      
+                created_at,      
+              updated_at,      
+                deleted_at          
+        FROM OPENJSON(@JsonData)          
+        WITH (          
+               grn_id  VARCHAR(36),      
+                line_num INT,      
+                po_line_num INT,      
+                po_shipment_num INT,      
+                item_code VARCHAR(50),      
+                item_desc NVARCHAR(MAX),      
+                qty_ordered DECIMAL(18,2),      
+                qty_received DECIMAL(18,2),      
+                qty_accepted DECIMAL(18,2),      
+                qty_rejected DECIMAL(18,2),      
+                uom VARCHAR(10),      
+                grn_line_id VARCHAR(36),      
+                unit_price DECIMAL(18,2),      
+                line_amount DECIMAL(18,2),      
+                destination_type VARCHAR(25),      
+                subinventory VARCHAR(30),      
+                locator_id INT,      
+                inspection_status VARCHAR(20),      
+                created_at datetime,    
+                updated_at datetime,      
+                deleted_at datetime                 )          
+            
+  --SELECT * FROM @TempLineItems    
+  --Update Existing LIne Items        
+   UPDATE TAR        
+   SET          
+              
+          po_line_num       = SRC.po_line_num,      
+                po_shipment_num   = SRC.po_shipment_num,      
+                item_code         = SRC.item_code,      
+                item_desc         = SRC.item_desc,      
+                qty_ordered       = SRC.qty_ordered,      
+                qty_received      = SRC.qty_received,      
+                qty_accepted      = SRC.qty_accepted,      
+                qty_rejected      = SRC.qty_rejected,      
+                uom               = SRC.uom,      
+                grn_line_id       = SRC.grn_line_id,      
+                unit_price        = SRC.unit_price,      
+                line_amount       = SRC.line_amount,      
+                destination_type  = SRC.destination_type,      
+                subinventory      = SRC.subinventory,      
+                locator_id        = SRC.locator_id,      
+                inspection_status = SRC.inspection_status,      
+                updated_at        = GETDATE()      
+ FROM @TempLineItems SRC        
+ JOIN Vertiv..GRNLineItems TAR         
+ ON  SRC.grn_line_id=TAR.grn_line_id            
+        
+ --Insert new Line Items        
+        
+        INSERT  INTO Vertiv..GRNLineItems        
+  (          
+                grn_id,      
+                line_num,      
+                po_line_num,      
+                po_shipment_num,      
+                item_code,      
+                item_desc,      
+                qty_ordered,      
+                qty_received,      
+                qty_accepted,      
+                qty_rejected,      
+                uom,      
+                grn_line_id,      
+                unit_price,      
+                line_amount,      
+                destination_type,      
+                subinventory,      
+                locator_id,      
+                inspection_status,      
+                created_at,    
+                updated_at,      
+                deleted_at          
+        )         
+  SELECT      
+               SRC.grn_id,      
+               SRC.line_num,      
+               SRC.po_line_num,      
+               SRC.po_shipment_num,      
+               SRC.item_code,      
+               SRC.item_desc,      
+               SRC.qty_ordered,      
+               SRC.qty_received,      
+               SRC.qty_accepted,      
+               SRC.qty_rejected,      
+               SRC.uom,      
+               SRC.grn_line_id,      
+               SRC.unit_price,      
+               SRC.line_amount,      
+               SRC.destination_type,      
+               SRC.subinventory,      
+               SRC.locator_id,      
+               SRC.inspection_status,      
+      ISNULL(SRC.created_at, GETDATE()),    
+               ISNULL(SRC.updated_at, GETDATE()),    
+               SRC.deleted_at     
+  FROM @TempLineItems SRC LEFT JOIN Vertiv..GRNLineItems PLI     ON SRC.grn_line_id = PLI.grn_line_id WHERE PLI.grn_line_id IS NULL;       
+        
+              
+  insert into VERTIV..POJsonfromERP          
+  values('GRNlineitem',@JsonData,getdate())          
+END   
